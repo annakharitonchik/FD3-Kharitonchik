ee8d99db94017c6625535a6aeebeed2136ec6dfd
Anna Kharitonchik <zharitonchik@icloud.com>
2025-06-22 19:13:54 +0300

    create ishop0

---
+++ App.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import Shop from "./components/Shop";
+
+ReactDOM.render(
+  <Shop
+    name="Ромашка"
+    address="г. Минск, ул. Полевая, 6"
+    time="9:00 - 18:00"
+  />,
+  document.getElementById("container")
+);
---
+++ components/Shop.js
@@ -0,0 +1,27 @@
+﻿import React from "react";
+
+import "./Shop.css";
+
+class Shop extends React.Component {
+  render() {
+    return (
+      <div className="shop">
+        <h1>Интернет-магазин</h1>
+        <span className="name">
+          <h3>Название интернет-магазина:</h3>
+          {this.props.name}
+        </span>
+        <span className="address">
+          <h3>Адрес:</h3>
+          {this.props.address}
+        </span>
+        <span className="time">
+          <h3>Время работы:</h3>
+          {this.props.time}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Shop;
