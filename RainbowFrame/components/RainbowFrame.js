479feaf1671e42f25a1f3500e90c60f998a7053c
Anna Kharitonchik <zharitonchik@icloud.com>
2025-07-24 15:41:44 +0300

    RainbowFrame

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,21 @@
+﻿import React from "react";
+
+class RainbowFrame extends React.Component {
+  render() {
+    let readyLayer = (
+      <p style={{ textAlign: "center", fontSize: "20px", fontWeight: "bold" }}>
+        {this.props.children}
+      </p>
+    );
+    this.props.colors.forEach((color, i) => {
+      readyLayer = (
+        <div style={{ border: ` solid 10px ${color}`, margin: `10px` }}>
+          {readyLayer}
+        </div>
+      );
+    });
+    return <div>{readyLayer}</div>;
+  }
+}
+
+export default RainbowFrame;
