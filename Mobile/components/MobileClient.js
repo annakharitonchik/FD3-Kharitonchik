bce5da7efa332ffae4bf2e06f2ef74c43d458ab3
Anna Kharitonchik <zharitonchik@icloud.com>
2025-08-02 03:25:00 +0300

    mobile

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,121 @@
+﻿import React from "react";
+import "./MobileClient.css";
+import PropTypes from "prop-types";
+import { clientEvents } from "./events";
+
+class MobileClient extends React.PureComponent {
+  static propTypes = {
+    info: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      surname: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      middleName: PropTypes.string.isRequired,
+      balance: PropTypes.number.isRequired,
+    }).isRequired,
+  };
+
+  surnameRef = React.createRef(); // return { current: undefined }
+  nameRef = React.createRef();
+  middleNameRef = React.createRef();
+  balanceRef = React.createRef();
+  state = {
+    isEdit: false,
+  };
+  startEdit = () => {
+    this.setState({ isEdit: true });
+  };
+
+  saveEdit = () => {
+    if (
+      this.surnameRef.current &&
+      this.nameRef.current &&
+      this.middleNameRef.current &&
+      this.balanceRef.current
+    ) {
+      let newSurname = this.surnameRef.current.value;
+      let newName = this.nameRef.current.value;
+      let newMiddleNname = this.middleNameRef.current.value;
+      let newBalance = Number(this.balanceRef.current.value);
+
+      clientEvents.emit("ETextChanged", {
+        id: this.props.info.id,
+        surname: newSurname,
+        name: newName,
+        middleName: newMiddleNname,
+        balance: newBalance,
+      });
+    }
+
+    this.setState({ isEdit: false });
+  };
+
+  render() {
+    console.log("MobileClient name = " + this.props.info.name + " render");
+    return (
+      <div className="Client">
+        {this.state.isEdit ? (
+          <React.Fragment>
+            <input
+              defaultValue={this.props.info.surname}
+              ref={this.surnameRef}
+            ></input>
+            <input
+              defaultValue={this.props.info.name}
+              ref={this.nameRef}
+            ></input>
+            <input
+              defaultValue={this.props.info.middleName}
+              ref={this.middleNameRef}
+            ></input>
+            <input
+              defaultValue={this.props.info.balance}
+              ref={this.balanceRef}
+            ></input>
+            <p
+              className={
+                this.props.info.balance > 0 ? "StatusActive" : "StatusBlocked"
+              }
+            >
+              {this.props.info.balance > 0 ? "active" : "blocked"}
+            </p>
+            <p>
+              <button className="Save" onClick={this.saveEdit}>
+                Сохранить
+              </button>
+            </p>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <p className="Surname">{this.props.info.surname}</p>
+            <p className="Name">{this.props.info.name}</p>
+            <p className="MiddleName">{this.props.info.middleName}</p>
+            <p className="Balance">{this.props.info.balance}</p>
+            <p
+              className={
+                this.props.info.balance > 0 ? "StatusActive" : "StatusBlocked"
+              }
+            >
+              {this.props.info.balance > 0 ? "active" : "blocked"}
+            </p>
+            <p>
+              <button className="Edit" onClick={this.startEdit}>
+                Редактировать
+              </button>
+            </p>
+          </React.Fragment>
+        )}
+
+        <p>
+          <button
+            className="Delete"
+            onClick={() => clientEvents.emit("EDelClient", this.props.info.id)}
+          >
+            Удалить
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,133 @@
+﻿import React from "react";
+import "./MobileCompany.css";
+import MobileClient from "./MobileClient.js";
+import PropTypes from "prop-types";
+import { clientEvents } from "./events";
+
+class MobileCompany extends React.PureComponent {
+  headers = [
+    "Фамилия",
+    "Имя",
+    "Отчество",
+    "Баланс",
+    "Статус",
+    "Редактировать",
+    "Удалить",
+  ];
+
+  static propTypes = {
+    clients: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        surname: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        middleName: PropTypes.string.isRequired,
+        balance: PropTypes.number.isRequired,
+      })
+    ),
+  };
+  state = {
+    clients: this.props.clients,
+    clientsFilter: "all", // active | blocked
+  };
+
+  componentDidMount = () => {
+    clientEvents.addListener("ETextChanged", this.textChanged);
+    clientEvents.addListener("EDelClient", this.deleteClient);
+  };
+
+  componentWillUnmount = () => {
+    clientEvents.removeListener("ETextChanged", this.textChanged);
+
+    clientEvents.removeListener("EDelClient", this.deleteClient);
+  };
+  textChanged = (info) => {
+    let { id, surname, name, middleName, balance } = info;
+    let newClients = this.state.clients.map((client) => {
+      if (id === client.id) {
+        return {
+          ...client,
+          surname,
+          name,
+          middleName,
+          balance,
+        };
+      }
+      return client;
+    });
+    this.setState({ clients: newClients });
+  };
+
+  deleteClient = (id) => {
+    this.setState({
+      clients: this.state.clients.filter((client) => id != client.id),
+    });
+  };
+  clientKey = 125;
+  addClient = () => {
+    this.setState({
+      clients: [
+        ...this.state.clients,
+        {
+          surname: "",
+          name: "",
+          middleName: "",
+          balance: 0,
+          id: this.clientKey,
+        },
+      ],
+    });
+    this.clientKey += 5;
+  };
+  render() {
+    let filteredClients = this.state.clients.filter((client) => {
+      if (this.state.clientsFilter === "active") return client.balance > 0;
+      if (this.state.clientsFilter === "blocked") return client.balance <= 0;
+      return true;
+    });
+    console.log("MobileCompany render");
+    const MobileClientsCode = filteredClients.map((client) => (
+      <MobileClient key={client.id} info={client} />
+    ));
+
+    return (
+      <div className="MobileCompany ">
+        <div className="ThreeButtons">
+          <button
+            onClick={() => {
+              this.setState({ clientsFilter: "all" });
+            }}
+          >
+            Все
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ clientsFilter: "active" });
+            }}
+          >
+            Активные
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ clientsFilter: "blocked" });
+            }}
+          >
+            Заблокированные
+          </button>
+        </div>
+        <div className="InfoHeaders">
+          {this.headers.map((header, index) => (
+            <h3 key={index + 10}>{header}</h3>
+          ))}
+        </div>
+        <div className="Clients">{MobileClientsCode}</div>
+
+        <button className="AddClient" onClick={this.addClient}>
+          Добавить клиента
+        </button>
+      </div>
+    );
+  }
+}
+
+export default MobileCompany;
